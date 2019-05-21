2c5dc9fe5fe01ea05925ef240cb8bed6179207ce
Alex Taddeo <44502930+VlexTvddeo@users.noreply.github.com>
2019-05-21 08:19:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//JavaScript here 
+//JavaScript here
 var score;
 var time;
 var numOne;
@@ -6,11 +6,24 @@ var numTwo;
 var addSub;
 var answer;
 var ansGiven;
-
+//setting start time
 var time = 20;
 
+//button disapear on start
+function butDis() {
+    document.getElementById("starter").style.cssText = 'opacity: 0';
+}
+
+//flash card visible
+function seeCard() {
+  document.getElementById("ele").style.cssText = 'width: 400';
+}
+
+//run timer down to 0
 function start(){
 var timeleft = 19;
+butDis ();
+seeCard ();
 var downloadTimer = setInterval(function(){
   document.getElementById("countdown").innerHTML = timeleft;
   timeleft -= 1;
@@ -18,12 +31,16 @@ var downloadTimer = setInterval(function(){
     clearInterval(downloadTimer);
     document.getElementById("countdown").innerHTML = "0";
   }
-  function startv2(){
-    
-  }
 }, 1000);
 }
 
+//random number generator
+function randoNum() {
+  return Math.floor(Math.random(0-9));
+  var numOne = randoNum
+}
+
+//adding numbers
 function adding(){
   var answer= numOne+numTwo;
 }
